3741c98c87118b1ea106436f47ac76cdc043f6ff
smzkjnpi3232 <kizmhny2561@gmail.com>
2024-11-20 14:56:40 +0900

    テキストレビュー②の回答を記述

--- test_app/connection.php
+++ test_app/connection.php
@@ -25,3 +25,30 @@ function getAllRecords()
     $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL';
     return $dbh->query($sql)->fetchAll();
 }
+
+// 更新処理
+function updateTodoData($post)
+{
+    $dbh = connectPdo();
+    $sql = 'UPDATE todos SET content = "' . $post['content'] . '" WHERE id = ' . $post['id'];
+    $dbh->query($sql);
+}
+//DBにすでに登録したデータを更新
+
+function getTodoTextById($id)
+{
+    $dbh = connectPdo();
+    $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL AND id =' . $id;
+    $data = $dbh->query($sql)->fetch();
+    return $data['content'];
+}
+//現在保存されているTODOの内容を返してくれています。
+
+//論理削除のDB処理
+function deleteTodoData($id)
+{
+    $dbh = connectPdo();
+    $now = date('Y-m-d H:i:s');
+    $sql = 'UPDATE todos SET deleted_at = "' . $now . '" WHERE id =' . $id;
+    $dbh->query($sql);
+}
--- test_app/functions.php
+++ test_app/functions.php
@@ -1,12 +1,43 @@
 <?php
 require_once('connection.php');
 
-function createData($post)
-{
-    createTodoData($post['content']);
-}
-
 function getTodoList()
 {
     return getAllRecords();
 }
+
+//更新処理
+function getSelectedTodo($id)
+{
+    return getTodoTextById($id);
+}
+
+function savePostedData($post)
+{
+    $path = getRefererPath();
+    switch ($path) {
+        case '/new.php':
+            createTodoData($post['content']);
+            break;
+        case '/edit.php':
+            updateTodoData($post);
+            break;
+        case '/index.php': // 追記
+            deleteTodoData($post['id']); // 追記
+            break; // 追記
+        default:
+            break;
+    }
+}
+
+//リクエスト元のページのパスで条件分岐をし、処理を振り分け
+//新規作成ページからPOSTされたなら、createTodoData関数 を実行（INSERT処理
+//編集ページからPOSTされたなら、updateTodoData関数 を実行（UPDATE処理）
+
+function getRefererPath()
+{
+    $urlArray = parse_url($_SERVER['HTTP_REFERER']);
+    return $urlArray['path'];
+}
+
+//リクエスト元のURLを文字列で取得しそのパスを返す
